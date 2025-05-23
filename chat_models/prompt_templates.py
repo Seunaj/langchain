8bd9aaab74f21928d4eb2f4ddf89eb2f6ab88711
Oluwaseun Ajayi <45903000+Seunaj@users.noreply.github.com>
2025-05-23 00:09:15 -0500

    Update and rename 1_prompt_templates_starter.py to prompt_templates.py

--- chat_models/prompt_templates.py
+++ chat_models/prompt_templates.py
@@ -6,18 +6,7 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4.1-nano")
 
-# template = "Write a {tone} email to {company} expressing interest in the {position} position, mentioning {skill} as a key strength. Keep it to 4 lines max"
-
-# prompt_template = ChatPromptTemplate.from_template(template)
-
-# prompt =  prompt_template.invoke({
-#     "tone": "energetic", 
-#     "company": "samsung", 
-#     "position": "AI Engineer", 
-#     "skill": "AI"
-# })
-
-# Example 2: Prompt with System and Human Messages (Using Tuples)
+# Prompt with System and Human Messages (Using Tuples)
 messages = [
     ("system", "You are an {role} in {organization}."),
     ("human", "Tell me something about {division} in {organization}."),
